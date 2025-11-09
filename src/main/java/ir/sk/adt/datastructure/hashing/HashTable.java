7d5d73ba3f325e36559ebf6f0e22cdafbf841a77
Saeid.keyvanfar <sad.keyvanfar@gmail.com>
2025-11-09 17:56:35 +0100

    add a comment

--- src/main/java/ir/sk/adt/datastructure/hashing/HashTable.java
+++ src/main/java/ir/sk/adt/datastructure/hashing/HashTable.java
@@ -7,7 +7,8 @@ import ir.sk.helper.pattern.HashingIndexPattern;
  *
  * A hash table is simply an array associated with a function (the hash function).
  * It efficiently implements the dictionary ADT with efficient insert, remove and find operations, each taking O(1)O(1) expected time.
- *
+ * hashing part is done by hash function that maps keys to positions in the array. and in that index we can store the value associated with that key.
+ * in this example we are just storing boolean values to indicate presence of a key.
  */
 @HashingIndexPattern
 public class HashTable {
